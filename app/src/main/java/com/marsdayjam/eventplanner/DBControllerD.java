a049373b50145a3bc46304692f3419e86bedb3a1
Beaster <watzzupman@hotmail.com>
2015-04-28 02:04:52 -0500

    before i go to sleep updating work

--- app/src/main/java/com/marsdayjam/eventplanner/DBControllerD.java
+++ app/src/main/java/com/marsdayjam/eventplanner/DBControllerD.java
@@ -7,14 +7,17 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteException;
 import android.util.Log;
 
-import com.marsdayjam.eventplanner.DBContract.EmployeeTable;
+
 import com.marsdayjam.eventplanner.DBContract.CalendarTable;
 import com.marsdayjam.eventplanner.DBContract.CalendarList;
+import com.marsdayjam.eventplanner.DBContract.EmployeeTable;
+import com.marsdayjam.eventplanner.DBContract.EventTable;
+
 
 import java.util.ArrayList;
 import java.util.List;
 
-/*
+
 public class DBControllerD {
     private static DBController ourInstance;
     private DBHelperD Helper;
@@ -26,7 +29,46 @@ public class DBControllerD {
     }
 
     // Handle Event Table operations
+    // Event and Calendar names must be unique(no doubles)
+    public long insertEvent(String eventName, String sDate, String eDate,
+                            String teamName, String calendarName) {
+        long id;
+        ContentValues values = new ContentValues();
+        values.put(EventTable.COLUMN_NAME_EVENTNAME, eventName);
+        values.put(EventTable.COLUMN_NAME_SDATE, sDate);
+        values.put(EventTable.COLUMN_NAME_EDATE, eDate);
+        values.put(EventTable.COLUMN_NAME_TEAMNAME, teamName);
+        values.put(EventTable.COLUMN_NAME_CALENDARNAME, calendarName);
+        id = db.insert(CalendarList.TABLE_NAME, null, values);
+        long cid = insertCalendar(calendarName);
+        return id; // negative if something went wrong, is row index otherwise
+    }
 
+    // returns an arraylist of the eventNames intended for easy listing of Active Events
+    public ArrayList<String> findAllEvents() {
+        String[] columns = {EventTable.COLUMN_NAME_EVENTNAME};
+        Cursor cursor = db.query(EventTable.TABLE_NAME, columns, null, null, null, null, null);
+        ArrayList<String> list = new ArrayList<String>();
+        while(cursor.moveToNext()){
+            int index  = cursor.getColumnIndex(EventTable.COLUMN_NAME_EVENTNAME);
+            String eventName = cursor.getString(index);
+            list.add(eventName);
+        }
+        return list;
+    }
+
+    public void deleteEvent(String eventName) {
+        String[] columns = {EventTable.COLUMN_NAME_EVENTNAME, EventTable.COLUMN_NAME_CALENDARNAME};
+        Cursor cursor = db.query(EventTable.TABLE_NAME, columns, EventTable.COLUMN_NAME_EVENTNAME+
+                                 " = '"+eventName+"'", null, null, null, null);
+        cursor.moveToNext();
+        int index = cursor.getColumnIndex(EventTable.COLUMN_NAME_CALENDARNAME);
+        String calendarName = cursor.getString(index);
+        deleteCalendar(calendarName);
+
+        String[] whereArgs = {eventName};
+        db.delete(EventTable.TABLE_NAME, EventTable.COLUMN_NAME_EVENTNAME+ "=?", whereArgs);
+    }
 
 
     // Handle Team Table operations
@@ -36,46 +78,79 @@ public class DBControllerD {
     // Handle CalendarList operations
     public long insertCalendar(String calendarName) {
         long id;
+        //arbitrary method
+        //createCalendarTable(calendarName);
         ContentValues values = new ContentValues();
         values.put(CalendarList.COLUMN_NAME_CALENDARNAME, calendarName);
         id = db.insert(CalendarList.TABLE_NAME, null, values);
-        return id;
+        return id; // negative if something went wrong, is row index otherwise
     }
 
-    public void deleteCalendar(String calendar) {
-        String[] whereArgs = {calendar};
+    public void deleteCalendar(String calendarName) {
+        //arbitrary method
+        //deleteCalendarTable(calendarName);
+        String[] whereArgs = {calendarName};
         db.delete(CalendarList.TABLE_NAME, CalendarList.COLUMN_NAME_CALENDARNAME+ "=?", whereArgs);
     }
 
+    // returns an arraylist of the calendarNames intended for easy calendar(Table) selection
+    public ArrayList<String> findAllCalendars() {
+        String[] columns = {CalendarList.COLUMN_NAME_CALENDARNAME};
+        Cursor cursor = db.query(CalendarList.TABLE_NAME, columns, null, null, null, null, null);
+        ArrayList<String> list = new ArrayList<String>();
+        while(cursor.moveToNext()){
+            int index = cursor.getColumnIndex(CalendarList.COLUMN_NAME_CALENDARNAME);
+            String calendarName = cursor.getString(index);
+            list.add(calendarName);
+        }
+        return list;
+    }
+
+
 
     // Handle CalendarEvent operations
-
-    public long insertCalendarEvent(String date, int start, int end, String event){
+    public long insertCalendarEvent(String date, int startH, int startM, int endH, int endM,
+                                    String event){
         long id;
         ContentValues values = new ContentValues();
         values.put(CalendarTable.COLUMN_NAME_DATE, date);
-        values.put(CalendarTable.COLUMN_NAME_START, start);
-        values.put(CalendarTable.COLUMN_NAME_END, end);
+        values.put(CalendarTable.COLUMN_NAME_STARTH, startH);
+        values.put(CalendarTable.COLUMN_NAME_STARTM, startM);
+        values.put(CalendarTable.COLUMN_NAME_ENDH, endH);
+        values.put(CalendarTable.COLUMN_NAME_ENDM, endM);
         values.put(CalendarTable.COLUMN_NAME_EVENT, event);
         id = db.insert(CalendarTable.TABLE_NAME, null, values);
-        return id;
+        return id; // negative if something went wrong, is row index otherwise
     }
 
-    public String findAllEvents(String date, int ) {
-        String[] columns = {CalendarTable.COLUMN_NAME_EVENT};
+    // returns a String of all events (including start and end time for each) for the date passed in
+    // date passed in must be in form given by Caldroid Calendar
+    public String findAllCalendarEvents(String date) {
+        String[] columns = {CalendarTable.COLUMN_NAME_EVENT, CalendarTable.COLUMN_NAME_STARTH,
+                            CalendarTable.COLUMN_NAME_STARTM, CalendarTable.COLUMN_NAME_ENDH,
+                            CalendarTable.COLUMN_NAME_ENDM};
         Cursor cursor = db.query(CalendarTable.TABLE_NAME, columns, null, null, null, null, null);
         StringBuffer buffer = new StringBuffer();
         while(cursor.moveToNext()){
-            String event = cursor.getColumnIndex(CalendarTable.COLUMN_NAME_EVENT)
-            buffer.append(event +"\n\n");
+            int index = cursor.getColumnIndex(CalendarTable.COLUMN_NAME_EVENT);
+            String event = cursor.getString(index);
+            index = cursor.getColumnIndex(CalendarTable.COLUMN_NAME_STARTH);
+            int startH = cursor.getInt(index);
+            index = cursor.getColumnIndex(CalendarTable.COLUMN_NAME_STARTM);
+            int startM = cursor.getInt(index);
+            index = cursor.getColumnIndex(CalendarTable.COLUMN_NAME_ENDH);
+            int endH = cursor.getInt(index);
+            index = cursor.getColumnIndex(CalendarTable.COLUMN_NAME_ENDM);
+            int endM = cursor.getInt(index);
+            buffer.append(event +"\n" +"\t"+startH+":"+startM+" - "+endH+":"+endM+"\n\n");
         }
         return buffer.toString();
     }
 
-    public void deleteEvents(String date) {
+    // Deletes all events for the Date sent in as argument(Must be in same form Caldroid Calander returns)
+    public void deleteCalendarsEvents(String date) {
         String[] whereArgs = {date};
         db.delete(CalendarTable.TABLE_NAME, CalendarTable.COLUMN_NAME_DATE+ "=?", whereArgs);
     }
 
 }
-*/
--- app/src/main/java/com/marsdayjam/eventplanner/DBHelperD.java
+++ app/src/main/java/com/marsdayjam/eventplanner/DBHelperD.java
@@ -11,7 +11,7 @@ import com.marsdayjam.eventplanner.DBContract.EventTable;
 import com.marsdayjam.eventplanner.DBContract.EmployeeTable;
 
 
-/*
+
 public class DBHelperD extends SQLiteOpenHelper {
     private static final String TEXT_TYPE = " TEXT";
     private static final String INT_TYPE = " INTEGER";
@@ -20,23 +20,26 @@ public class DBHelperD extends SQLiteOpenHelper {
     private static final String RP = " ) ";
 
     private static final String CREATE_CALENDAR_LIST =
-            "CREATE TABLE " + DBContract.CalendarList.TABLE_NAME + LP +
-                    DBContract.CalendarList.COLUMN_NAME_CALENDARNAME + TEXT_TYPE + RP;
+            "CREATE TABLE " + CalendarList.TABLE_NAME + LP +
+                    CalendarList.COLUMN_NAME_CALENDARNAME + TEXT_TYPE + RP;
 
     private static final String CREATE_CALENDAR =
-            "CREATE TABLE " + DBContract.CalendarTable.TABLE_NAME + LP +
+            "CREATE TABLE " + CalendarTable.TABLE_NAME + LP +
                     CalendarTable._ID + " INTEGER PRIMARY KEY" + CM +
                     CalendarTable.COLUMN_NAME_DATE + TEXT_TYPE + CM +
-                    CalendarTable.COLUMN_NAME_START + INT_TYPE + CM +
-                    CalendarTable.COLUMN_NAME_END + INT_TYPE + CM +
+                    CalendarTable.COLUMN_NAME_STARTH + INT_TYPE + CM +
+                    CalendarTable.COLUMN_NAME_STARTM + INT_TYPE + CM +
+                    CalendarTable.COLUMN_NAME_ENDH + INT_TYPE + CM +
+                    CalendarTable.COLUMN_NAME_ENDM + INT_TYPE + CM +
                     CalendarTable.COLUMN_NAME_EVENT + TEXT_TYPE + RP;
 
     private static final String CREATE_EVENT =
-            "CREATE TABLE " + DBContract.CalendarTable.TABLE_NAME + LP +
+            "CREATE TABLE " + CalendarTable.TABLE_NAME + LP +
                     EventTable._ID + " INTEGER PRIMARY KEY" + CM +
+                    EventTable.COLUMN_NAME_EVENTNAME + TEXT_TYPE + CM +
                     EventTable.COLUMN_NAME_SDATE + TEXT_TYPE + CM +
                     EventTable.COLUMN_NAME_EDATE + TEXT_TYPE + CM +
-                    EventTable.COLUMN_NAME_TEAMS+ TEXT_TYPE + CM +
+                    EventTable.COLUMN_NAME_TEAMNAME + TEXT_TYPE + CM +
                     EventTable.COLUMN_NAME_CALENDARNAME + TEXT_TYPE + RP;
 
     public static int DATABASE_VERSION = 1;
@@ -45,12 +48,13 @@ public class DBHelperD extends SQLiteOpenHelper {
     public DBHelperD(Context context) { super(context, DATABASE_NAME, null, DATABASE_VERSION); }
 
     public void onCreate(SQLiteDatabase db) {
+        /*
         ContentValues values;
 
-        db.execSQL(CREATE_CALENDAR_LIST);
+        //db.execSQL(CREATE_CALENDAR_LIST);
 
         values = new ContentValues();
-        values.put(CalendarList.COLUMN_NAME_CALENDARNAME, EmployeeTable.COLUMN_NAME_CALENDARNAME);
+        */
     }
 
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
@@ -61,4 +65,3 @@ public class DBHelperD extends SQLiteOpenHelper {
         DATABASE_VERSION++;
     }
 }
-*/
