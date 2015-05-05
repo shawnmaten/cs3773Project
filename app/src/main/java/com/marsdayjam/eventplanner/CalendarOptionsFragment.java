37cf16c06755e30a90b74634ddcce6504acc8453
shawnaten <shawnmaten@gmail.com>
2015-05-05 12:12:06 -0500

    making changes to calendar child fragments

--- app/src/main/java/com/marsdayjam/eventplanner/CalendarOptionsFragment.java
+++ app/src/main/java/com/marsdayjam/eventplanner/CalendarOptionsFragment.java
@@ -1,64 +1,38 @@
 package com.marsdayjam.eventplanner;
 
-import android.annotation.SuppressLint;
-import android.support.v4.app.Fragment;
-import android.content.Context;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-
+import android.widget.Toast;
 
 import java.util.Date;
 
 import static android.view.View.OnClickListener;
 
-
-@SuppressLint("ValidFragment")
-public class CalendarOptionsFragment extends Fragment{
-
-    private static CalendarFragment calendarFragment;
-    private static Context context;
-    private static Boolean dateSelected = Boolean.FALSE;
-    private static Date prevDate;
-    private static Button edit, viewDelete;
-
-
-    public View onCreateView( LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_calendar_options, container, false);
-        this.edit = (Button) view.findViewById (R.id.editCalendar);
-        edit.setOnClickListener(new OnClickListener(){
+public class CalendarOptionsFragment extends Fragment {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        final CalendarFragment calendarFragment = (CalendarFragment) getParentFragment();
+        View root = inflater.inflate(R.layout.fragment_calendar_options, container, false);
+        root.findViewById (R.id.add_event).setOnClickListener(new OnClickListener(){
             @Override
             public void onClick(View v) {
-                if(dateSelected == Boolean.TRUE){
-                    calendarFragment.addEvent();
-                }
+                Date selectedDate = calendarFragment.getSelectedDate();
+                Toast.makeText(getActivity(),
+                        selectedDate != null ? selectedDate.toString() : "NULL",
+                        Toast.LENGTH_SHORT).show();
             }
         });
-        this.viewDelete = (Button) view.findViewById (R.id.viewEvents);
-        viewDelete.setOnClickListener(new OnClickListener(){
+        root.findViewById (R.id.view_events).setOnClickListener(new OnClickListener(){
             @Override
             public void onClick(View v) {
-                if(dateSelected == Boolean.TRUE){
-                    calendarFragment.viewEvent();
-                }
+                Toast.makeText(getActivity(),
+                        String.format("# Events: %d", calendarFragment.getEvents().size()),
+                        Toast.LENGTH_SHORT).show();
             }
         });
-        return view;
-    }
-
-    @SuppressLint("ValidFragment")
-    public CalendarOptionsFragment(CalendarFragment calendarFragment, Context context){
-        this.calendarFragment = calendarFragment;
-        this.context = context;
-    }
-
-    public void setDateSelected(Boolean dateSelected) {
-        this.dateSelected = dateSelected;
-    }
-
-    public static void setContext(Context context) {
-        CalendarOptionsFragment.context = context;
+        return root;
     }
 }
